e6f0dd26fc5427b65cc0d70f27431081c3ecbe39
Morgan Fudge <mfudge92@gmail.com>
2017-10-05 05:29:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(cardNumber == "") {
-    console.log("Sorry, we don\'t have a credit card on file for you.")
+    console.log("Sorry, we don't have a credit card on file for you.")
     return false
   }
+  console.log(`Your total cost is ${total()}, which will be charged to the c
+ard 39326499.'`)
 }
