f06a9b59b777ae2bc48f58aa2963b6b5c6c2ace0
Morgan Fudge <mfudge92@gmail.com>
2017-10-05 05:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,6 +63,5 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
     return false
   }
-  console.log(`Your total cost is ${total()}, which will be charged to the c
-ard 39326499.'`)
+  console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.'`)
 }
