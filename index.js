5ac6570dacefd3fc8e24f0d6d12435d91c380aa9
Morgan Fudge <mfudge92@gmail.com>
2017-10-05 05:13:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
   let str = "In your cart, you have "
   for(let item in cart) {
     if(item == 0) {
-      str += `${Object.keys(cart[item])[0]} at $${cart[item]}}`
+      str += `${Object.keys(cart[item])[0]} at $${cart[item][Object.keys(cart[item])[0]]}}`
     } else if(item == cart.length - 1) {
 
     } else {
